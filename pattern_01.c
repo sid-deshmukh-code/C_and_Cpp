c5ad38e68839ea4f584eed741258e3b510b93728
Siddhant2601 <deshmukhsiddhant420@gmail.com>
2022-07-15 10:00:19 -0500

    pattern_01

---
+++ pattern_01.c
@@ -0,0 +1,28 @@
+# include<stdio.h>
+
+
+/* 
+
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+
+*/
+
+
+int main(){
+
+    int i, j;
+    for(i=1;i<=5;i++){
+        for (j = 1; j <= 5; j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+        
+    }
+
+
+}
