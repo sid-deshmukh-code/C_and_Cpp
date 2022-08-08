d14bb059f79e52a67c55c58a53cbcc46e983a2d6
Siddhant2601 <deshmukhsiddhant420@gmail.com>
2022-08-08 11:00:39 -0500

    added boilerplate code for copy pasters

---
+++ C++/Boilerplate.cpp
@@ -0,0 +1,13 @@
+// C++ Boilercode for ultimate copy-pasters
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of program begins
+int main()
+{
+	// Your code here
+
+	return 0;
+}
