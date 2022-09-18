18e6defd2d95d9f77ad03a7d089d6d9e2112ae2b
siddhant <deshmukhsiddhant420@gmail.com>
2022-09-18 12:20:30 -0500

    added a program

--- C++/Classes/classss.cpp
+++ C++/Classes/classss.cpp
@@ -1,1 +1,40 @@
-// Class in C++
+// C++ code for " Class concept "
+// Author : Siddhant Deshmukh
+
+#include <iostream>
+using namespace std;
+
+class student
+{
+private:
+    int roll_no;
+    float math,phy,total;
+public:
+    void getdata()
+    {
+        cout <<"Enter the roll no and marks"<<endl;
+        cin >> roll_no >> math >> phy;
+
+    }
+
+    void display()
+    {
+        total = math + phy;
+        cout << "Roll no " <<roll_no << " Total marks " << total;
+
+    }
+};
+
+
+
+int main()
+{
+
+
+	student s1;
+    s1.getdata();
+    s1.display();
+
+    
+	return 0;
+}
